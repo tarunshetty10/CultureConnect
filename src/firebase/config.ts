24c7c5951c19b98f9dba62d394a9809dd32d5a95
Tarun Shetty <tshetty2020@gmail.com>
2026-02-12 12:24:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7784532433-72b62",
+  "appId": "1:10796431370:web:0154d88984000d630287f3",
+  "apiKey": "AIzaSyDZuQhZ6j7uZ7NotJVRkjlTXnSBkgBP7TU",
+  "authDomain": "studio-7784532433-72b62.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "10796431370"
+};
