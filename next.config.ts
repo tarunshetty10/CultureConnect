fef8bffaeb46f4f2ba7d8fd238e9da307de39ff4
Tarun Shetty <tshetty2020@gmail.com>
2026-02-20 10:31:49 +0000

    Use this URL https://cdn.99pandit.com/images/blogsimg/ganesh%20puja%203.

--- next.config.ts
+++ next.config.ts
@@ -37,6 +37,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.99pandit.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
